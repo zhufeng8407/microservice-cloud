fcdca926c12656f723f59750ad6e52775653c8b9
zhufeng <zuhfeng.zhu@163.com>
2019-07-31 23:36:00 +0800

    cloud commit

---
+++ microservice-simple-consumer-movie/src/main/java/com/feng/cloud/consumer/controller/MovieController.java
@@ -0,0 +1,13 @@
+package com.feng.cloud.consumer.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class MovieController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+}
