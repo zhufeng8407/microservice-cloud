4105027cda4ba8579d722b5276d0b9a1530186d3
zhufeng <zuhfeng.zhu@163.com>
2019-08-04 00:25:33 +0800

    cloud commit

---
+++ microservice-cloud-eureka-server7001/src/main/java/com/feng/cloud/eureka/EurekaServer7001.java
@@ -0,0 +1,15 @@
+package com.feng.cloud.eureka;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer7001 {
+
+    public static void main(String[] args) {
+        new SpringApplication().run(EurekaServer7001.class, args);
+    }
+}
