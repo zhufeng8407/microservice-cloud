4f8de47c6e9caad8ca62da18f967bd11092e6e49
zhufeng8407 <zhufeng.zhu@163.com>
2019-08-06 17:31:27 +0800

    add zuul

---
+++ microservice-simple-zuul/src/main/java/com/feng/cloud/zuul/ZuulApplication.java
@@ -0,0 +1,10 @@
+package com.feng.cloud.zuul;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulApplication {
+}
