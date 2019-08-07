9410219d98e932ee7b801ebe2cca2aad5af4d1d1
zhufeng8407 <zhufeng.zhu@163.com>
2019-08-07 14:24:38 +0800

    add hystrix

--- microservice-simple-consumer-movie/src/main/java/com/feng/cloud/consumer/controller/MovieController.java
+++ microservice-simple-consumer-movie/src/main/java/com/feng/cloud/consumer/controller/MovieController.java
@@ -2,6 +2,7 @@ package com.feng.cloud.consumer.controller;
 
 import com.feng.cloud.consumer.entity.User;
 import com.feng.cloud.consumer.feign.UserFeignClient;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ public class MovieController {
     @Autowired
     private UserFeignClient userFeignClient;
 
+    @HystrixCommand(fallbackMethod = "fallBackFindUser")
     @GetMapping("/simple/{id}")
     public User findUserById(@PathVariable Long id) {
         return restTemplate.getForObject("http://localhost:8001/simple/" + id, User.class);
@@ -26,6 +28,12 @@ public class MovieController {
     @GetMapping("/client/{id}")
     public User findUserById2(@PathVariable Long id) {
         return userFeignClient.findById(id);
+    }
+
+    public User fallBackFindUser(Long id) {
+        User user = new User();
+        user.setName("太拥挤了，请稍等！！");
+        return user;
     }
 
 }
---
+++ microservice-simple-zuul/src/main/java/com/feng/cloud/zuul/filter/TokenFilter.java
@@ -0,0 +1,33 @@
+package com.feng.cloud.zuul.filter;
+
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class TokenFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return FilterConstants.PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return FilterConstants.PRE_DECORATION_FILTER_ORDER - 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return false;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext context = RequestContext.getCurrentContext();
+        HttpServletRequest request = context.getRequest();
+        return null;
+    }
+}
